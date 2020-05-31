9c918f94d7a3aa3ad00aca0fd75d18a5793d4951
Dwyriel <Dwyriel333@hotmail.com>
2020-05-31 09:56:33 -0300

    Startup to Collisions

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,35 +7,33 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rb;
     AudioSource aS;
-    float np, nm;
+    [SerializeField] float rcsThrust = 110f, mainThrust = 750f;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         aS = GetComponent<AudioSource>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        np = 110 * Time.deltaTime;
-        nm = (-1) * np;
         Thrust();
         Rotate();
     }
 
     private void Thrust()
     {
+        float thrust = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(Vector3.up * thrust);
             if (!aS.isPlaying) aS.Play();
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rb.AddRelativeForce(Vector3.down);
+            rb.AddRelativeForce(Vector3.down * thrust);
             if (!aS.isPlaying) aS.Play();
         }
         else
@@ -46,19 +44,40 @@ public class Rocket : MonoBehaviour
 
     private void Rotate()
     {
+        float rotation = rcsThrust * Time.deltaTime;
         rb.freezeRotation = true; //pauses rotation physics
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
-            print("Ignore A & D");
+
+            transform.Rotate(0, 0, 0, Space.World);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0, 0, nm, Space.World);
+            transform.Rotate(0, 0, -rotation, Space.World);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0, 0, np, Space.World);
+            transform.Rotate(0, 0, rotation, Space.World);
         }
         rb.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Not ded");
+                break;
+            case "Fuel":
+                print("got fuel");
+                break;
+            case "Finish":
+                print("win");
+                break;
+            default:
+                print("die");
+                break;
+        }
     }
 }
